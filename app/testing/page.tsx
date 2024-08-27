3cb84dd166406bd4e4c63343d9e1e7ee7ef24574
farhan <chfarhanilyas550@gmail.com>
2024-08-27 09:49:37 -0700

    Updated for Sir TRA checking untill module 1 frontend

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -1,13 +1,56 @@
-"use client";
+import React from 'react';
+import ShinyButton from "../components/MagicUI/ShinyButton";
 
-import React, { useState } from 'react';
+import { Button } from "../components/ShadeCN/Button"
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../components/ShadeCN/AlertDialog"
 
 const TestComponent: React.FC = () => {
  
-
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className='bg-slate-300 h-screen'>
+      <h1 className='p-12'>Hello</h1>
+
+      <ShinyButton text="Shiny" className='bg-red-500 hover:bg-red-600' />
+
+      <div className='p-6'>
+        <Button className='bg-red-500 hover:bg-red-600'>Button</Button>
+        <br /> <br />
+        <hr />
+        <Button variant="destructive" >Button</Button>
+      </div>
+
+
+      <div className='m-32'>
+        <AlertDialog>
+          <AlertDialogTrigger>Open</AlertDialogTrigger>
+          <AlertDialogContent className='bg-red-500'>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete your account
+                and remove your data from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </div>
+
+
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,3 +25,46 @@ const config: Config = {
   plugins: []
 }
 export default config
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import type { Config } from 'tailwindcss'
+
+// const config: Config = {
+//   content: [
+//     './pages/**/*.{js,ts,jsx,tsx,mdx}',
+//     './components/**/*.{js,ts,jsx,tsx,mdx}',
+//     './app/**/*.{js,ts,jsx,tsx,mdx}',
+//     './src/**/*.{js,ts,jsx,tsx,mdx}',
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         sans: ['Poppins', 'sans-serif'],
+//       },
+//       colors: {
+//         farhan: "#FF4500",
+//         polo: "#FF5733",
+//         sada: "#ff7b66",
+//         pay: "#00bc99",
+//         yoyo: "#8A2BE2",
+        
+//       },
+//     },
+//   },
+//   plugins: []
+// }
+// export default config
