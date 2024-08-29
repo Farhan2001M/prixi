507c21231b4fb7e35bc85d265ef258b485440780
farhan <chfarhanilyas550@gmail.com>
2024-08-29 06:04:18 -0700

    tested UI libraries like Daisy NEXT and Accernity Succesfully

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,9 +18,6 @@ const config: Config = {
       colors: {
         farhan: "#FF4500",
         polo: "#FF5733",
-        sada: "#ff7b66",
-        pay: "#00bc99",
-        yoyo: "#8A2BE2",
       },
     },
   },
@@ -31,46 +28,3 @@ const config: Config = {
   ]
 }
 export default config
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import type { Config } from 'tailwindcss'
-
-// const config: Config = {
-//   content: [
-//     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-//     './components/**/*.{js,ts,jsx,tsx,mdx}',
-//     './app/**/*.{js,ts,jsx,tsx,mdx}',
-//     './src/**/*.{js,ts,jsx,tsx,mdx}',
-//   ],
-//   theme: {
-//     extend: {
-//       fontFamily: {
-//         sans: ['Poppins', 'sans-serif'],
-//       },
-//       colors: {
-//         farhan: "#FF4500",
-//         polo: "#FF5733",
-//         sada: "#ff7b66",
-//         pay: "#00bc99",
-//         yoyo: "#8A2BE2",
-        
-//       },
-//     },
-//   },
-//   plugins: []
-// }
-// export default config
